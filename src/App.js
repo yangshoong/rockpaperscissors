50eab63225db3d88bdc816f4f79c9d7bb9e1df2a
yangshoong <crinaeae83@gmail.com>
2024-03-20 00:42:15 +0900

    react userchoice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Box from "./component/Box";
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://letstalkscience.ca/sites/default/files/2020-02/balanced-stones-on-a-pebble-beach.jpg"
+
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://www.sallybeauty.co.uk/dw/image/v2/BBTX_PRD/on/demandware.static/-/Sites-sally-master-catalog/default/dw5f0fe95b/images/original/815144_1.jpg?sw=1000&sh=1000"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://5.imimg.com/data5/HC/KF/HZ/SELLER-4619430/white-art-paper.jpg"
+  }
+}
+
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <Box title="You" item={userSelect}/>
+        {/* <Box title="Computer" /> */}
+      </div>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/component/Box.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Box = (props) => {
+    return (
+        <div className="box">
+            <h1>{props.title}</h1>
+            <img className="item-img" src={props.item && props.item.img}></img>
+            <h2>WIN</h2>
+
+        </div>
+    )
+}
+
+export default Box
