9acd2f89884ad384c2bf8bc0ac71433f279f8097
yangshoong <crinaeae83@gmail.com>
2024-03-20 00:49:28 +0900

    react userchoice_2

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
     return (
         <div className="box">
             <h1>{props.title}</h1>
-            <img className="item-img" src={props.item && props.item.img}></img>
+            <img className="item-img" src={props.item && props.item.img} alt=""></img>
             <h2>WIN</h2>
 
         </div>
