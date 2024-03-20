3d501a732e21737f58f6ebdbffd88f46bd963290
Yangshoong <crinaeae83@gmail.com>
2024-03-20 18:17:36 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -21,17 +21,45 @@ const choice = {
 
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("")
+  const [comResult, setComResult] = useState("")
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+    setComResult(judgement(computerChoice, choice[userChoice]));
+
+
   }
 
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie"
+    } else if (user.name === "Rock") return computer.name === "Scissors" ? "win" : "lose"
+    else if (user.name === "Scissors") return computer.name === "Paper" ? "win" : "lose"
+    else if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose"
+
+  }
+
+
+
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+
+  }
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computer" /> */}
+        <Box title="You" item={userSelect} result={result} box={result} />
+        <Box title="Computer" item={computerSelect} result={comResult} box={comResult} />
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Box = (props) => {
     return (
-        <div className="box">
+        <div className={props.box}>
             <h1>{props.title}</h1>
             <img className="item-img" src={props.item && props.item.img} alt=""></img>
-            <h2>WIN</h2>
+            <h2>{props.result}</h2>
 
         </div>
     )
